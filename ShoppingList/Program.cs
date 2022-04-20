646968634d1bedb66c352ee9889373571e0164ec
Carmel Meelind <cmeelind@tktk.ee>
2022-04-21 00:20:13 +0300

    add new projects

---
+++ ShoppingList/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace ShoppingList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string rootDirectory = @"C:\Users\Carmel\source\repos\Nädal 8 - failid-20220412";
+            Console.WriteLine("Enter directory name:");
+            string newDirectory = Console.ReadLine();
+            Console.WriteLine("Enter file name;");
+            string fileName = Console.ReadLine();
+
+            if (Directory.Exists($"{rootDirectory}\\{newDirectory}") && File.Exists($"{rootDirectory}\\{newDirectory}\\{fileName}"))
+            {
+                Console.WriteLine($"Directory and File exists.");
+            }
+            else
+            {
+                Directory.CreateDirectory($"{rootDirectory}\\{newDirectory}");
+                File.Create($"{rootDirectory}\\{newDirectory}\\{fileName}").Close();
+            }
+
+            string fileLocation = rootDirectory + "\\" + newDirectory + "\\" + fileName;
+
+            string[] arrayFromFile = File.ReadAllLines(fileLocation);
+            List<string> myShoppingList = arrayFromFile.ToList<string>();
+
+            bool loopActive = true;
+
+            while (loopActive)
+            {
+                Console.WriteLine("Would you like to add an item? Y/N:");
+                char userInput = Convert.ToChar(Console.ReadLine().ToLower());
+
+                if (userInput == 'y')
+                {
+                    Console.WriteLine("Enter your item:");
+                    string userItem = Console.ReadLine();
+                    myShoppingList.Add(userItem);
+                }
+                else
+                {
+                    loopActive = false;
+                    Console.WriteLine("Take care!");
+                }
+            }
+
+            Console.Clear();
+
+            foreach (string item in myShoppingList)
+            {
+                Console.WriteLine($"Your item: {item}");
+            }
+
+            File.AppendAllLines(fileLocation, myShoppingList);
+
+        }
+    }
+}
